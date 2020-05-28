7051be1207d1b9270122672bb147887cf154c742
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2020-05-28 16:56:29 +0900

    Add files via upload

---
+++ src/sogongdo/productPackageManagement/entity/Member.java
@@ -0,0 +1,62 @@
+package sogongdo.memberManagememt.entity;
+
+public class Member 
+{
+	private String memberid;
+	private String memberpw;
+	private String membername;
+	private String phonenum;
+	private String email;
+	private String address;
+	
+	public String getId()
+	{
+		return memberid;		
+	}
+	public String getPw()
+	{
+		return memberpw;
+	}
+	public String getName()
+	{
+		return membername;
+	}
+	public String getPhonenum()
+	{
+		return phonenum;	
+	}
+	public String getEmail()
+	{
+		return email;		
+	}
+	public String getAddress()
+	{
+		return address;		
+	}
+	
+	public void setId(String memberid)
+	{
+		this.memberid=memberid;		
+	}
+	public void setpw(String memberpw)
+	{
+		this.memberpw=memberpw;
+	}
+	public void setName(String membername)
+	{
+		this.membername=membername;
+	}
+	public void setPhonenum(String phonenum)
+	{
+		this.phonenum=phonenum;	
+	}
+	public void setEmail(String email)
+	{
+		this.email=email;		
+	}
+	public void setAddress(String address)
+	{
+		this.address=address;
+	}
+	
+	}
